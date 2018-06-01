09a4f4374e80dfe5dc6a5fa49f24454163845d11
Daniel Schäfer <schaefer.dev@gmail.com>
2018-06-01 15:06:32 +0200

    example code to set zumo speed

--- project01/src/collector/main.cpp
+++ project01/src/collector/main.cpp
@@ -1,9 +1,11 @@
 #include <Arduino.h>
+#include "../../libs/libzumo/Zumo32U4Motors.h"
 
 void setup() {
 
 }
 
 void loop() {
-
+    Zumo32U4Motors test = Zumo32U4Motors();
+    test.setLeftSpeed(0);
 }
