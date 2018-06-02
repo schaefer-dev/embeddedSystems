6481a70785ba52257a6726c9b9720111adec0394
kvnmlr <kevin.mueller1@live.de>
2018-06-02 02:19:41 +0200

    include basic serial stuff again

--- project01/src/collector/main.cpp
+++ project01/src/collector/main.cpp
@@ -4,6 +4,8 @@
 DifferentialDrive motor = DifferentialDrive(10, 10, 0, 0, 0);
 int16_t leftSpeed = 50;
 int16_t rightSpeed = -50;
+int ByteReceived;
+
 
 void drive() {
     motor.drive(leftSpeed, rightSpeed);
@@ -12,11 +14,27 @@ void drive() {
 void setup() {
     DifferentialDrive::setLeftSpeed(leftSpeed);
     DifferentialDrive::setRightSpeed(rightSpeed);
+
+    Serial1.begin(9600);
+    Serial1.println("--- Start Serial Monitor ---");
+    Serial1.println("(Decimal)(Hex)(Character)");
+    Serial1.println();
 }
 
 void loop() {
     drive();
-    delay(1000);
+
+    if (Serial1.available() > 0)
+    {
+        ByteReceived = Serial1.read();
+
+        Serial1.print(ByteReceived);
+        Serial1.print("        ");
+        Serial1.print(ByteReceived, HEX);
+        Serial1.print("       ");
+        Serial1.print(char(ByteReceived));
+        Serial1.println();
+    }
 }
 
 
