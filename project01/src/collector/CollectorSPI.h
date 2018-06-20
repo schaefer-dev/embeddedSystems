6df3a676fa3504e24ecf774720358dc8d01ef7c7
Daniel Schäfer <schaefer.dev@gmail.com>
2018-06-20 20:31:40 +0200

    SPI Stuff for collector, interrupt makes SPI extremly slow right now

--- project01/src/collector/CollectorSPI.h
+++ project01/src/collector/CollectorSPI.h
@@ -1,30 +1,27 @@
 //
-// Created by rafael on 06.06.18.
+// Created by Daniel Schäfer on 20.06.18.
 //
 
-#ifndef EMBEDDEDSYSTEMS18_SPIMASTER_H
-#define EMBEDDEDSYSTEMS18_SPIMASTER_H
+#ifndef EMBEDDEDSYSTEMS18_COLLECTORSPI_H
+#define EMBEDDEDSYSTEMS18_COLLECTORSPI_H
 
 #include <stdint.h>
 
 #define SLAVE_NONE 0
-#define SLAVE_ADC 1
-#define SLAVE_RF 2
+#define SLAVE_RF 1
 
-class ScoutSPI
-{
+
+class CollectorSPI {
 
 private:
     static void waitNextSPIRisingEdge();
     static void waitNextSPIFallingEdge();
-    static void waitNextADCRisingEdge();
-    static void waitNextADCFallingEdge();
     static unsigned int readWriteSPI(unsigned int payload);
 
 
 public:
 
-    ScoutSPI();
+    CollectorSPI();
 
     static void SPIMasterInit();
 
@@ -33,17 +30,13 @@ public:
     static int readADC(char sensorAdress);
     static void initializeRFModule();
 
-    static void setTimer1Interrupt(uint16_t factor);
-
-    static void ADCConversionWait();
+    static void setTimer4Interrupt(uint16_t duration);
 
     static unsigned int interruptCounter;
 
     static volatile unsigned char SPIClock;
-    static volatile unsigned char ADCClock;
 
     static bool runSPIClock;
-
 
     static int queryRFModule();
     static void debug_RFModule();
@@ -51,4 +44,4 @@ public:
 };
 
 
-#endif
+#endif //EMBEDDEDSYSTEMS18_COLLECTORSPI_H
